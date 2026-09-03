b350a9c023dba7fc4f7119853bfae19dfc1db9f4
Gitoldor <rajguruowais@gmail.com>
2026-09-03 19:05:18 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,43 @@
+const posts = [
+  {
+    id: 1,
+    title: "welcome",
+    topic: "Scenic Drives",
+    type: "",
+    subtype: "",
+    lat: 15.9041,
+    lng: 73.8219,
+    image: "https://i.ibb.co/chqGP3bQ/SAWANT-WADI-20260831-173946-0000.png",
+    link: "welcome.html",
+    plan: "FREE",
+    expires: "2026-09-10"
+  },
+
+  {
+    id: 2,
+    title: "welcome",
+    topic: "Scenic Drives",
+    type: "",
+    subtype: "",
+    lat: 15.9941,
+    lng: 73.8219,
+    image: "https://i.ibb.co/chqGP3bQ/SAWANT-WADI-20260831-173946-0000.png",
+    link: "welcome.html",
+    plan: "PRO",
+    expires: "2026-09-02"
+  },
+
+  {
+    id: 3,
+    title: "welcome",
+    topic: "Scenic Drives",
+    type: "",
+    subtype: "",
+    lat: 15.9541,
+    lng: 73.8219,
+    image: "https://i.ibb.co/chqGP3bQ/SAWANT-WADI-20260831-173946-0000.png",
+    link: "welcome.html",
+    plan: "PREMIUM",
+    expires: "2026-09-20"
+  }
+];
