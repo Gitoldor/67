4cc4dc7224d87f1573c3e4c78cb95a3fc56bf6f6
Raj <h>
2026-09-07 15:42:04 +0530

    update

--- sw.js
+++ sw.js
@@ -1,54 +1,48 @@
-const CACHE_NAME = "my-app-v1";
-
-const STATIC_FILES = [
-  "./",
-  "index.html",
-  "manifest.json",
-  "offline.html"
+const CACHE_NAME = 'mypwa-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  'https://raw.githubusercontent.com/google/material-design-icons/master/png/action/home/materialicons/24dp/2x/baseline_home_black_24dp.png',
+  'https://raw.githubusercontent.com/google/material-design-icons/master/png/action/home/materialicons/48dp/2x/baseline_home_black_48dp.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(STATIC_FILES))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-
-        if (response.ok) {
-          const copy = response.clone();
-
-          caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request, copy);
-          });
-        }
-
-        return response;
-      })
-      .catch(() => {
-        return caches.match(event.request)
-          .then(cached => {
-            return cached || caches.match("./offline.html");
-          });
-      })
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).then((fetchResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    }).catch(() => {
+      if (event.request.mode === 'navigate') {
+        return caches.match('/index.html');
+      }
+    })
   );
 });
